69e7e5c4f3ed89452b7136f30bce9238dc4dc70f
haaroon <haaroon@ovex.io>
2023-04-05 15:22:55 +0200

    models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,11 +1,23 @@
+from datetime import datetime
+
+from apps.shared.models import db
+from apps.user.models import User
 
 
-# todo article model
 # todo holds content info and how to render it
-# todo one - many tags
-# todo one - one authors (user of type author)
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-# todo tag model
-# todo many - one articles
-# todo metadata on which to annotate searches
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    article_id = db.Column(db.Integer, db.ForeignKey(Article.id))
