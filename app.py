df0fc577f421b4f32cf27b61662f30206891db78
haaroon <haaroon@ovex.io>
2023-04-05 10:22:13 +0200

    config app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from apps.shared.app import migrate, login_manager
@@ -6,6 +8,8 @@ from apps.shared.models import db
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
