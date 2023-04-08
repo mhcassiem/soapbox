79e0c270f44b38e930ee5e2ded2cd791bc2c3858
haaroon <haaroon@ovex.io>
2023-04-08 22:14:29 +0200

    register auth app and base app

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import os
 
 from flask import Flask
 
-from apps.shared.app import migrate, login_manager
+from apps.shared.app import migrate
+from apps.auth.app import login_manager
 from apps.shared.models import db
+from apps.auth.routes import auth
+from apps.shared.routes import shared
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.register_blueprint(shared)
+    app.register_blueprint(auth)
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
