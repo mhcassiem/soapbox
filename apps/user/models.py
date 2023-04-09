f26e09e3bf3f376ccaf91c7a6742b124a0545f2e
haaroon <haaroon@ovex.io>
2023-04-09 08:27:44 +0200

    user token fields

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,7 @@
+import base64
 import enum
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,8 +17,8 @@ class MemberType(enum.Enum):
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, index=True, unique=True)
+    username = db.Column(db.String, nullable=False, index=True, unique=True)
     password = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String, nullable=True)
     last_name = db.Column(db.String, nullable=True)
@@ -25,12 +27,37 @@ class User(db.Model, UserMixin):
     member_type = db.Column(db.Enum(MemberType), default='user')
     updated_at = db.Column(db.DateTime, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return f"User {self.email}"
+
+    def to_dict(self):
+        return dict(id=self.id, email=self.email)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+# todo set token expiration from env
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
