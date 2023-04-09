471568733ca89748e8b7c12e95bea6542eb86331
haaroon <haaroon@ovex.io>
2023-04-09 08:30:51 +0200

    register apps

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask
 
+from apps.shared.models import db
 from apps.shared.app import migrate
 from apps.auth.app import login_manager
-from apps.shared.models import db
-from apps.auth.routes import auth
 from apps.shared.routes import shared
+from apps.auth.routes import auth
+from apps.api.routes import api
+from apps.error_handlers.routes import error_handlers
 
 
 def create_app():
@@ -14,7 +16,9 @@ def create_app():
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.register_blueprint(shared)
+    app.register_blueprint(error_handlers)
     app.register_blueprint(auth)
+    app.register_blueprint(api, url_prefix='/api')
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
