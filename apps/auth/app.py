eabc319d600b44d8d572f91f999c2df3a2289530
haaroon <haaroon@ovex.io>
2023-04-09 08:28:24 +0200

    api basic auth and token auth

--- apps/auth/app.py
+++ apps/auth/app.py
@@ -1,4 +1,5 @@
-from apps.shared.app import login_manager
+from apps.api.errors import error_response
+from apps.shared.app import login_manager, basic_auth, token_auth
 from apps.user.models import User
 
 
@@ -7,4 +8,23 @@ def load_user(user_id: str):
     return User.query.get(int(user_id))
 
 
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
 
+
+@basic_auth.error_handler
+def basic_auth_error(status):
+    return error_response(status)
+
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+
+@token_auth.error_handler
+def token_auth_error(status):
+    return error_response(status)
--- apps/shared/app.py
+++ apps/shared/app.py
@@ -1,5 +1,8 @@
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
 login_manager = LoginManager()
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 migrate = Migrate()
