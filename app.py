49d25ae1baaba05d93b82e8e08247a405a17fb7c
haaroon <haaroon@ovex.io>
2023-04-09 12:58:04 +0200

    init jwt

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 from apps.shared.models import db
-from apps.shared.app import migrate
+from apps.shared.app import migrate, jwt
 from apps.auth.app import login_manager
 from apps.shared.routes import shared
 from apps.auth.routes import auth
@@ -21,5 +21,6 @@ def create_app():
     app.register_blueprint(api, url_prefix='/api')
     db.init_app(app)
     login_manager.init_app(app)
+    jwt.init_app(app)
     migrate.init_app(app, db)
     return app
