136d98c81f0383cc7af1ef417f0df935cc6cb39c
haaroon <haaroon@ovex.io>
2023-04-12 08:57:33 +0200

    remove flask login

--- apps/shared/app.py
+++ apps/shared/app.py
@@ -2,9 +2,8 @@ from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
-login_manager = LoginManager()
+
 basic_auth = HTTPBasicAuth()
 jwt = JWTManager()
 migrate = Migrate()
