754b7747f735ae14c9b3c873e10415ef91acf3cd
haaroon <haaroon@ovex.io>
2023-04-15 08:19:32 +0200

    minor cleaning up imports

--- apps/shared/app.py
+++ apps/shared/app.py
@@ -1,10 +1,8 @@
-from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 
 
-basic_auth = HTTPBasicAuth()
 jwt = JWTManager()
 migrate = Migrate()
 mail = Mail()
