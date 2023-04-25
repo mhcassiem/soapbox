1126e1fbe81a880dfe4292c21f301215cf85b18f
haaroon <haaroon@ovex.io>
2023-04-25 17:27:04 +0200

    added graphql

---
+++ apps/graphql_api/routes.py
@@ -0,0 +1,31 @@
+from ariadne import make_executable_schema, graphql_sync, gql, load_schema_from_path
+from ariadne.explorer import ExplorerGraphiQL
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
+
+from configs import Config
+from graphql_api.mutations import mutation
+from graphql_api.queries import query
+
+graphql = Blueprint('graphql_api', __name__)
+type_defs = gql(load_schema_from_path('schema.graphql'))
+schema = make_executable_schema(type_defs, query, mutation)
+explorer_html = ExplorerGraphiQL().html(None)
+
+
+@graphql.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=Config.DEBUG
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+@graphql.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return explorer_html, 200
